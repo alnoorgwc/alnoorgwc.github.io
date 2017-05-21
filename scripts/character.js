543123fd2637b7aee21f73ad6f61d7549067f926
Elizabeth Cannon <betsy@polaris.local>
2017-05-21 14:56:56 -0400

    Scatter in semicolons

--- scripts/character.js
+++ scripts/character.js
@@ -1,15 +1,15 @@
 // Randomly select a character
-character = Math.random()
+character = Math.random();
 
 var link;
 if (character <= 0.25){
-	link = 'https://www.actimator.com/app/play/angwc-bw'
+	link = 'https://www.actimator.com/app/play/angwc-bw';
 } elif (character <= 0.5){
-	link = 'https://www.actimator.com/app/play/angwc-bm'
+	link = 'https://www.actimator.com/app/play/angwc-bm';
 } elif (character <= 0.75){
-	link = 'https://www.actimator.com/app/play/angwc-ww'
+	link = 'https://www.actimator.com/app/play/angwc-ww';
 } else {
-	link = 'https://www.actimator.com/app/play/angwc-wm'
+	link = 'https://www.actimator.com/app/play/angwc-wm';
 }
 
-document.getElementById('char-link').src = link
+document.getElementById('char-link').src = link;
