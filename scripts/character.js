6f7f9df9c856f12d5d112e4ee9960c6444bb2033
Elizabeth Cannon <betsy@polaris.local>
2017-05-21 15:07:52 -0400

    Else if

--- scripts/character.js
+++ scripts/character.js
@@ -4,9 +4,9 @@ character = Math.random();
 var link;
 if (character <= 0.25){
 	link = 'https://www.actimator.com/app/play/angwc-bw';
-} elif (character <= 0.5){
+} else if (character <= 0.5){
 	link = 'https://www.actimator.com/app/play/angwc-bm';
-} elif (character <= 0.75){
+} else if (character <= 0.75){
 	link = 'https://www.actimator.com/app/play/angwc-ww';
 } else {
 	link = 'https://www.actimator.com/app/play/angwc-wm';
