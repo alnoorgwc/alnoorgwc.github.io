38b1c245feff17b28a6816091431dd39f015a480
Elizabeth Cannon <betsy@polaris.local>
2017-05-21 14:51:27 -0400

    Reserved word

--- scripts/character.js
+++ scripts/character.js
@@ -1,12 +1,12 @@
 // Randomly select a character
-char = Math.random()
+character = Math.random()
 
 var link;
-if (char =< 0.25){
+if (character =< 0.25){
 	link = 'https://www.actimator.com/app/play/angwc-bw'
-} elif (char =< 0.5){
+} elif (character =< 0.5){
 	link = 'https://www.actimator.com/app/play/angwc-bm'
-} elif (char =< 0.75){
+} elif (character =< 0.75){
 	link = 'https://www.actimator.com/app/play/angwc-ww'
 } else {
 	link = 'https://www.actimator.com/app/play/angwc-wm'
